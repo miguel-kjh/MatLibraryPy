c972e977967a477884ea0761789498c97752fd4f
miguel-kjh <miguelangel.kjh@gmail.com>
2021-01-07 17:47:00 +0000

    first commit

---
+++ cmake-build-debug/CMakeFiles/MatLibraryPy.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/miguel/Escritorio/SIANI/1 Cuatrimestre/PP/MatLibraryPy/main.cpp" "/home/miguel/Escritorio/SIANI/1 Cuatrimestre/PP/MatLibraryPy/cmake-build-debug/CMakeFiles/MatLibraryPy.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MatLibraryPy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatLibraryPy.dir/main.cpp.o"
+  "MatLibraryPy"
+  "MatLibraryPy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatLibraryPy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
